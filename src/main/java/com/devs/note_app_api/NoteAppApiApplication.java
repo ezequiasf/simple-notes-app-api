3e7ca7b847e0ad9b8410daf566aba18098bea6c7
Ezequias Barros <ezequiasbarros1997@gmail.com>
2026-02-27 13:36:22 -0300

    Configuracao inicial

---
+++ src/main/java/com/devs/note_app_api/NoteAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.devs.note_app_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteAppApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devs/note_app_api/NoteAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devs.note_app_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
