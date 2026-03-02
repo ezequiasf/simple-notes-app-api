0f57805818a87fe22ece4463ada696d3216a7739
Ezequias Barros <ezequiasbarros1997@gmail.com>
2026-03-02 15:22:51 -0300

    @Repository

--- src/main/java/com/devs/note_app_api/NoteRepository.java
+++ src/main/java/com/devs/note_app_api/NoteRepository.java
@@ -1,10 +1,11 @@
 package com.devs.note_app_api;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface NoteRepository extends JpaRepository<Note, Long> {
-
     Optional<Note> findNoteById(Long id);
 }
